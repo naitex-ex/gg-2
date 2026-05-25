29dae6105967f75712391ce4fb4fa5378c1aa9e6
naitex-ex <naitex762@gmail.com>
2026-05-25 18:52:07 +0530

    Update mainactivity.java

--- mainactivity.java
+++ mainactivity.java
@@ -1,1 +1,14 @@
-h
+package com.example.helloworld;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Connects the Java logic to the XML layout resource
+        setContentView(R.layout.activity_main);
+    }
+}
